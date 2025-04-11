1fe28c0b830b204179fef82c27bacf6d582ee32b
Ryan <r.davies220987@gmail.com>
2025-04-11 17:52:22 +1000

    added a pause to batch file so errors can be viewed

--- makepdf.bat
+++ makepdf.bat
@@ -3,3 +3,4 @@ REM Assumes Pandoc has PDF engine (e.g., via MiKTeX install)
 
 pandoc --output=OED-Ryan-Expanded.pdf --pdf-engine=xelatex --variable mainfont="Arial" --table-of-contents introduction.md core-mechanic.md characters.md equipment.md gameplay.md npcs.md rewards.md monsters.md spells.md traps.md resources.md x-additional-material.md x-campaign-material.md x-design-notes.md
 
+@pause
